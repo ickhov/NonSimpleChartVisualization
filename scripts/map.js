3cdd6c3b9c8a8ce3cb6dabfc48fd607646413b05
Iev Khov <ievkhov1@gmail.com>
2019-02-21 15:59:39 -0800

    updated dendogram

--- scripts/map.js
+++ scripts/map.js
@@ -1,4 +1,4 @@
-
+var treeMap = {};
 var svg = d3.select("#map");
 
 var boundingBox = svg.node().getBoundingClientRect();
@@ -17,7 +17,7 @@ var stateCode = {
     'Colorado': 'CO',
     'Connecticut': 'CT',
     'Delaware': 'DE',
-    'District Of Columbia': 'DC',
+    'District of Columbia': 'DC',
     'Federated States Of Micronesia': 'FM',
     'Florida': 'FL',
     'Georgia': 'GA',
@@ -82,23 +82,18 @@ function draw(error, data) {
     // important: First argument it expects is error
     if (error) throw error;
 
-    var names = {};
+    var astronautsData = data[1];
+    var stateCount = {};
 
-    var da = data[1];
+    astronautsData.forEach(function(d) {
+        var index = d.State;
 
-    d3.tsv("./data/us_state_name.tsv", function(error, data) {
-        data.forEach(function(d) {
-            //d.State = stateCode[d.State];
-            names[d.id] = d.code;
-        });
+        if (stateCount[index] == undefined) {
+            stateCount[index] = 1;
+        } else {
+            stateCount[index] = stateCount[index] + 1;
+        }
     });
-
-    data.forEach(function(d, i) {
-        //d.State = stateCode[d.State];
-        //names[i] = (d.features[i]).properties.name;
-    });
-
-    var x = d3.scaleLinear().range([1, 455]);
 
     // D3 Projection
     var projection = d3.geoAlbersUsa()
@@ -108,54 +103,68 @@ function draw(error, data) {
     var path = d3.geoPath()
         .projection(projection);
 
-    var colorScheme = d3.schemeBlues[9];
-
     // title for the bubble chart
     svg.append("text")
-        .attr("x", width / 2.6)
-        .attr("y", height / 12)
-        .attr("font-size", "30px")
-        .text("U.S. Astronauts by State");
-    // track clicking
-    var isClicked = false;
+        .attr("x", width / 4)
+        .attr("y", height / 15)
+        .attr("font-size", "24px")
+        .attr("fill", "white")
+        .attr("font-weight", "bold")
+        .text("Number of U.S. Astronauts per State");
+
     var stateSelected = {};
-
-    // define the tooltip behavior
-    var tooltip = d3.select('#myTooltip');
-    tooltip.style('display', 'none');
-
-    // show tooltip function
-    var showTooltip = function(d) {
-        // make sure our tooltip is going to be displayed
-        tooltip.style('display', 'block');
-
-        // set the initial position of the tooltip
-        tooltip.style('left', d3.event.pageX + 'px');
-        tooltip.style('top', d3.event.pageY + 'px');
-
-        tooltip.html("<strong>" + stateCode[d.properties.name] + "<strong>");
-    }
-
-    // hide tooltip function
-    var hideTooltip = function(d) {
-        tooltip.style('display', 'none');
-    }
+    var size = 0;
 
     // handle mouse click
     var mouseClick = function(d) {
-        // if stateSelected is empty
-        if (!isClicked) {
-            isClicked = true;
-            map.attr("opacity", 0.2);
-            label.attr("opacity", 0.2);
+        var index = stateCode[d.properties.name];
+        // stateSelected is empty && the current clicked state is not highlighted
+        // first state clicked so make all labels and map opaque out
+        if (size == 0 && stateSelected[index] == undefined) {
+            map.attr("opacity", 0.5);
+            label.attr("opacity", 0.5);
             d3.select(this).attr("opacity", 1);
-            d3.select("#" + d.properties.name).attr("opacity", 1);
-        } else {
-            isClicked = false;
+            d3.select("#" + index).attr("opacity", 1);
+            stateSelected[index] = d.properties.name;
+            size++;
+            addState(index);
+            updateTree("remove", "CA");
+            updateTree("add", index);
+        }
+        // stateSelected is not empty && the current clicked state is not highlighted
+        else if (size > 0 && stateSelected[index] == undefined) {
+            d3.select(this).attr("opacity", 1);
+            d3.select("#" + index).attr("opacity", 1);
+            stateSelected[index] = d.properties.name;
+            size++;
+            addState(index);
+            updateTree("add", index);
+        }
+        // stateSelected is not empty && the current clicked state is already highlighted
+        else if (size > 1 && stateSelected[index] != undefined) {
+            d3.select(this).attr("opacity", 0.5);
+            d3.select("#" + index).attr("opacity", 0.5);
+            stateSelected[index] = undefined;
+            size--;
+            removeState(index);
+            updateTree("remove", index);
+        }
+        // user deselected the last state
+        else if (size == 1 && stateSelected[index] != undefined){
             map.attr("opacity", 1);
             label.attr("opacity", 1);
+            stateSelected[index] = undefined;
+            size--;
+            stateNames = ["CA"];
+            updateTree("remove", index);
+            updateTree("add", "CA");
         }
     }
+
+    // color scale for the legend
+    var color = d3.scaleThreshold()
+        .domain([0, 5, 10, 15, 20, 25, 30])
+        .range(d3.schemeGreens[7]);
 
     // create and append the map of the US
     var map = svg.selectAll('path')
@@ -163,32 +172,157 @@ function draw(error, data) {
         .enter()
         .append('path')
         .attr('d', path)
-        .attr("fill", function(d, i) {
-            return colorScheme[i % 9];
+        .attr("fill", function(d){
+            var index = stateCode[d.properties.name];
+
+            if (stateCount[index] == undefined) {
+                stateCount[index] = 0;
+            }
+
+            return color(stateCount[index]);
         })
-        .style('stroke', 'black')
+        .style('stroke', 'white')
         .style('stroke-width', 0.75)
         .on("click", mouseClick);
+        
 
-    var label = svg.append("g")
-        .attr("class", "states-names")
-        .selectAll("text")
+    var label = svg.selectAll("g")
         .data(data[0].features)
         .enter()
+        .append("g")
         .append("text")
         .attr("id", function(d){
-            return d.properties.name;
-        })
-        .text(function(d, i){
             return stateCode[d.properties.name];
+        })
+        .attr("text-anchor","middle")
+        .attr('fill', function(d){
+
+            if (stateCount[stateCode[d.properties.name]] < 20) {
+                return "black";
+            }
+
+            return "white";
+        })
+        .attr("font-size", "13px")
+        .attr("pointer-events", "none");
+
+    // state name of label
+    label.append("tspan")
+        .text(function(d){
+            var index = stateCode[d.properties.name];
+
+            if (stateCount[index] == undefined) {
+                stateCount[index] = 0;
+            }
+
+            return index;
         })
         .attr("x", function(d){
             return path.centroid(d)[0];
         })
         .attr("y", function(d){
-            return  path.centroid(d)[1];
+            return  path.centroid(d)[1] - 7;
+        });
+
+    // astronaut count of label
+    label.append("tspan")
+        .text(function(d){
+            var index = stateCode[d.properties.name];
+
+            if (stateCount[index] == undefined) {
+                stateCount[index] = 0;
+            }
+
+            return stateCount[index];
         })
-        .attr("text-anchor","middle")
-        .attr('fill', 'black')
-        .attr("font-size", "13px");
+        .attr("x", function(d){
+            return path.centroid(d)[0];
+        })
+        .attr("y", function(d){
+            return  path.centroid(d)[1] + 7;
+        });
+
+    // handle mouse click
+    var reset = function(d) {
+        map.attr("opacity", 1);
+        label.attr("opacity", 1);
+        stateSelected = {};
+        size = 0;
+        clearAll();
+    }
+
+    var g = svg.append("g")
+        .attr("transform", function(d) { 
+            return "translate(" + width / 5.7 + "," + (height * 0.03) + ")"; 
+        })
+        .on("click", reset);
+
+    g.append("rect")
+        .attr("width", 50)
+        .attr("height", 20);
+
+    g.append("text")
+        .attr("x", 8)
+        .attr("y", 15)
+        .text("Reset")
+        .attr("fill", "white")
+        .attr("cursor", "pointer");
+
+    var treeBox = d3.select("body").select("#treeBox");
+
+    treeBox.append("p")
+        .html("Names of U.S. Astronaut in the Selected State");
+
+    var stateNames = ["CA"];
+
+    treeMap["CA"] = new TreeMap("CA", data[1]);
+
+    var clearAll = function(d) {
+        d3.selectAll(".tree").remove();
+    }
+
+    // clear the state from tree map
+    var clearStateFromTree = function(name) {
+        d3.select("#" + name + ".tree").remove();
+    }
+
+    // indicate whether to remove the preset state data
+    var firstClick = true;
+    var stateIndex = 0;
+
+    // add the state to list of state names
+    var addState = function(name) {
+        if (firstClick) {
+            stateNames = [];
+            firstClick = false;
         }
+
+        stateNames[stateIndex] = name;
+        stateIndex++;
+    }
+
+    // remove the state from the list of state names
+    var removeState = function(name) {
+        var index = findIndexOfState(name);
+        stateNames.splice(index, 1);
+        stateIndex--;
+    }
+
+    // update the tree to match current selection
+    var updateTree = function(operation, state) {
+        
+        if (operation == "add") {
+            treeMap[state] = new TreeMap(state, data[1]);
+        } else if (operation == "remove") {
+            treeMap[state] = null;
+            clearStateFromTree(state);
+        }
+    }
+
+    // find the index where the state name is at
+    var findIndexOfState = function(name) {
+        return stateNames.findIndex(function(obj) {
+            return obj == name;
+        });
+    }
+}
---
+++ scripts/tree.js
@@ -0,0 +1,294 @@
+
+function TreeMap(stateName, data) {
+
+    var height;
+
+    // filter data for tree
+    data = data.filter(function(d) {
+        return d.State == stateName;
+    })
+
+    var length = data.length - 1;
+    if (length < 5)
+        height = 300;
+    else if (length < 10)
+        height = 500;
+    else if (length < 15)
+        height = 700;
+    else if (length < 20)
+        height = 900;
+    else if (length < 25)
+        height = 1100;
+    else
+        height = 1300;
+
+    var treeBox = d3.select("body").select("#treeBox");
+    
+    var newTree = treeBox.append("svg")
+        .attr("width", "900px")
+        .attr("height", height + "px")
+        .attr("class", "tree")
+        .attr("id", stateName);
+
+    var svg = newTree.append("g")
+        .attr("transform", "translate(" + 150 + ",0)");
+
+    var boundingBox = newTree.node().getBoundingClientRect();
+
+    //  grab the width and height of our containing SVG
+    var width = boundingBox.width;
+
+    var treeData = {}, children = [];
+    treeData["name"] = stateName;
+
+    for (var i = 0; i < data.length; i++)
+    {
+        var map2 = {},
+        children2 = [];
+
+        var uMajor;
+        var gMajor;
+
+        if (data[i].UndergraduateMajor == "") {
+            uMajor = "N/A";
+        } else {
+            uMajor = data[i].UndergraduateMajor;
+        }
+
+        if (data[i].GraduateMajor == "") {
+            gMajor = "N/A";
+        } else {
+            gMajor = data[i].GraduateMajor;
+        }
+
+        map2["name"] = data[i].Name;
+        children2[0] = {"name": "Gender: " + data[i].Gender};
+        children2[1] = {"name": "Alma Mater: " + data[i].AlmaMater};
+        children2[2] = {"name": "Undergraduate Major: " + uMajor};
+        children2[3] = {"name": "Graduate Major: " + gMajor};
+        children2[4] = {"name": "Current Status: " + data[i].Status};
+        map2["children"] = children2;
+
+        children[i] = map2;
+    }
+
+    treeData["children"] = children;
+
+    // declares a tree layout and assigns the size
+    var treemap = d3.tree().size([height, width]);
+
+    var i = 0,
+        duration = 750,
+        root;
+
+    // Assigns parent, children, height, depth
+    root = d3.hierarchy(treeData, function(d) { return d.children; });
+    root.x0 = height / 2;
+    root.y0 = 0;
+
+    // Collapse after the second level
+    root.children.forEach(collapse);
+
+    update(root);
+
+    // Collapse the node and all it"s children
+    function collapse(d) {
+        if(d.children) {
+            d.tempChildren = d.children
+            d.tempChildren.forEach(collapse)
+            d.children = null
+        }
+    }
+
+    function update(source) {
+        // Assigns the x and y position for the nodes
+        var treeData = treemap(root);
+      
+        // Compute the new tree layout.
+        var nodes = treeData.descendants(),
+            links = treeData.descendants().slice(1);
+      
+        // Normalize for fixed-depth.
+        nodes.forEach(function(d) { 
+            d.y = d.depth * 180
+        });
+
+        // Id for each node for enter, update, and exit purposes
+        var keyID = function(d) {
+            if (d.id == undefined)
+            {
+                d.id = i;
+                i++;
+            }
+
+            return d.id;
+        }
+      
+        // Update the nodes...
+        var node = svg.selectAll("g.node")
+            .data(nodes, keyID);
+      
+        // Enter any new modes at the parent"s previous position.
+        var entering = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+              return "translate(" + source.y0 + "," + source.x0 + ")";
+            })
+            .on("click", toggle);
+
+        // Toggle children on click.
+        function toggle(d) {
+            // children is not null = children not hidden
+            // make them hidden
+            if (d.children) {
+                d.tempChildren = d.children;
+                d.children = null;
+            } 
+            // children is null = chidren is hidden
+            // unhide them
+            else {
+                d.children = d.tempChildren;
+                d.tempChildren = null;
+            }
+            update(d);
+        }
+      
+        // Add Circle for the nodes
+        entering.append("circle")
+            .attr("class", "node");
+      
+        // Add labels for the nodes
+        entering.append("text")
+            .attr("dy", ".35em")
+            .attr("fill", "white")
+            .attr("x", function(d) {
+                // if either the children or parent is visible
+                // shift x by -13
+                if (d.children || d.tempChildren) {
+                    return -13;
+                }
+
+                return 13;
+            })
+            .attr("text-anchor", function(d) {
+                // if either the children or parent is visible
+                // anchor text to "end"
+                if (d.children || d.tempChildren) {
+                    return "end";
+                }
+
+                return "start";
+            })
+            .text(function(d) { 
+                return d.data.name; 
+            });
+      
+        // UPDATE
+        var updating = entering.merge(node);
+      
+        // Transition to the proper position for the node
+        updating.transition()
+            .duration(duration)
+            .attr("transform", function(d) { 
+                return "translate(" + d.y  + "," + d.x + ")";
+            });
+      
+        // Update the node attributes and style
+        updating.select("circle.node")
+            .attr("r", 10)
+            .style("fill", function(d) {
+                // not null = child hidden
+                if (d.tempChildren) {
+                    return "#ee5200";
+                }
+                
+                return "white";
+            })
+            .attr("cursor", "pointer");
+      
+      
+        // Remove any exiting nodes
+        var exiting = node.exit().transition()
+            .duration(duration)
+            .attr("transform", function(d) {
+                return "translate(" + source.y + "," + source.x + ")";
+            })
+            .remove();
+      
+        // On exit reduce the node circles size to 0
+        exiting.select("circle")
+            .attr("r", 0);
+      
+        // On exit reduce the opacity of text labels
+        exiting.select("text")
+            .style("fill-opacity", 0);
+      
+        // Update the links...
+        var link = svg.selectAll("path.link")
+            .data(links, keyID);
+      
+        var init = {x: source.x0, y: source.y0};
+
+        // Enter any new links at the parent"s previous position.
+        var linkEntering = link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", diagonal(init, init));
+      
+        // UPDATE
+        var linkUpdating = linkEntering.merge(link);
+      
+        // Transition back to the parent element position
+        linkUpdating.transition()
+            .duration(duration)
+            .attr("d", function(d){ 
+                return diagonal(d, d.parent) 
+            });
+      
+        var end = {x: source.x, y: source.y};
+
+        // Remove any exiting links
+        link.exit().transition()
+            .duration(duration)
+            .attr("d", diagonal(end, end))
+            .remove();
+
+        // Creates a curved (diagonal) path from parent to the child nodes
+        function diagonal(s, d) {
+            return "M " + s.y + " " + s.x +
+                "C " + (s.y + d.y) / 2 + " " + s.x + "," +
+                (s.y + d.y) / 2 + " " + d.x + "," +
+                d.y + " " + d.x;
+        }
+      
+        // Store the old positions for transition.
+        nodes.forEach(function(d){
+            d.x0 = d.x;
+            d.y0 = d.y;
+        });
+    }
+
+    // handle mouse click
+    var reset = function(d) {
+        // Collapse after the second level
+        root.children.forEach(collapse);
+
+        update(root);
+    }
+
+    var g = newTree.append("g")
+        .attr("transform", function(d) { 
+            return "translate(" + 50 + "," + (root.x0 - 10) + ")"; 
+        })
+        .on("click", reset);
+
+    g.append("rect")
+        .attr("width", 50)
+        .attr("height", 20);
+
+    g.append("text")
+        .attr("x", 8)
+        .attr("y", 15)
+        .text("Reset")
+        .attr("fill", "white")
+        .attr("cursor", "pointer");
+}
